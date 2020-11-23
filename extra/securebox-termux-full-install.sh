aebcd3e5c94b4c5ce1f08a6743e453190187e541
Emerson Rocha <rocha@ieee.org>
2020-11-23 07:53:04 -0300

    securebox-termux-full-setup.sh (#4) improved documentation

--- extra/securebox-termux-full-install.sh
+++ extra/securebox-termux-full-install.sh
@@ -45,34 +45,38 @@ set -e
 
 #### How to run this script from a new installed Termux, END ___________________
 if command -v termux-setup-storage  ; then
-  echo "securebox-termux-full-setup.sh: OK. Detected Termux"
+  echo ">>> securebox-termux-full-setup.sh: OK. Detected Termux"
 else
-  echo "securebox-termux-full-setup.sh: ERROR! This setup is mean to run o Termux"
-  echo "Aborting"
+  echo ">>> securebox-termux-full-setup.sh: ERROR! This setup is mean to run o Termux"
+  echo "    Aborting"
   exit 1
 fi
 
-echo "securebox-termux-full-setup.sh: pkg update"
+echo ">>> securebox-termux-full-setup.sh: pkg update"
 yes | pkg update -y
-echo "securebox-termux-full-setup.sh: pkg upgrade"
+echo ">>> securebox-termux-full-setup.sh: pkg upgrade"
 yes | pkg upgrade -y
-echo "securebox-termux-full-setup.sh: pkg install -y git rsync (dependencies)"
+echo ">>> securebox-termux-full-setup.sh: pkg install -y git rsync (dependencies)"
 yes | pkg install -y git rsync
 
-#### termux-setup-storage ______________________________________________________
+#### termux-setup-storage (disabled) ___________________________________________
 # @see https://wiki.termux.com/wiki/Internal_and_external_storage
 # @see https://wiki.termux.com/wiki/Termux-setup-storage
 #  "If termux-setup-storage appears to do nothing, try rebooting your device and run the command again.""
 #   "Execute termux-setup-storage (run apt update && apt upgrade to make sure that this tool is available) to ensure:"
 
-echo "securebox-termux-full-setup.sh: termux-setup-storage"
-echo "securebox-termux-full-setup.sh: Your Android will ask to allow termux access to storage"
-termux-setup-storage
-sleep 10
-
 # TODO: tecnicacly speaking, if we only use "$HOME/backups" the
 #       termux-setup-storage command is not need. Review this on future
 #       (fititnt, 2020-11-19 13:12 UTC)
+# TODO: the next lines are commented. If need we can uncoment and allow more
+#       complex setups on the future. But just using $HOME (on termux, $PREFIX)
+#       can still be an alternative (fititnt, 2020-11-23 10:50 UTC)
+
+# echo ">>> securebox-termux-full-setup.sh: termux-setup-storage"
+# echo ">>> securebox-termux-full-setup.sh: Your Android will ask to allow termux access to storage"
+# termux-setup-storage
+# sleep 10
+
 
 #### Install (master, lastest version) from GitHub _____________________________
 ### Download to temporary ......................................................
@@ -86,14 +90,21 @@ cp -r "$TMPDIR/securebox/bin/" "$PREFIX/bin"
 #        storage) we could also add ~/storage/external-1 instead of ~/ on
 #        the search path (fititnt, 2020-11-19 13:07 UTC)
 
+echo ">>> securebox-termux-full-setup.sh: preparing $HOME/backups"
 mkdir "$HOME/backups" || echo "$HOME/backups already exists"
 chmod 0700 "$HOME/backups"
+
+echo ">>> securebox-termux-full-setup.sh: preparing $HOME/backups/tmp"
 mkdir "$HOME/backups/tmp" || echo "$HOME/backups/tmp already exists"
 chmod 0700 "$HOME/backups/tmp"
+
+echo ">>> securebox-termux-full-setup.sh: preparing $HOME/backups/mirror"
 mkdir "$HOME/backups/mirror" || echo "$HOME/backups/mirror already exists"
 chmod 0700 "$HOME/backups/mirror"
+
+echo ">>> securebox-termux-full-setup.sh: preparing $HOME/backups/archives"
 mkdir "$HOME/backups/archives" || echo "$HOME/backups/archives already exists"
 chmod 0700 "$HOME/backups/archives"
 
-echo "securebox-termux-full-setup.sh: For more information about storage, see"
-echo "https://wiki.termux.com/wiki/Internal_and_external_storage"
+echo ">>> securebox-termux-full-setup.sh: For more information about storage, see"
+echo "    https://wiki.termux.com/wiki/Internal_and_external_storage"
